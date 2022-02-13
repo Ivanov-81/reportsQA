3d37b2e932c8d07745b4c0dab2da71ac08736fd4
Oleg Ivanov <gmiolegv@gmail.com>
2022-02-13 23:31:15 +0300

    Second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,8 @@
 import React, {useEffect, useRef} from 'react';
-import clsx from 'clsx';
-import {makeStyles, TextField, Theme, withStyles} from "@material-ui/core";
 
 import './App.css'
 
-const useStyles = makeStyles((theme: Theme) => ({
-    root: {
-        maxWidth: '100%',
-    }
-}));
-
 function App() {
-
-    const resInput = useRef(null)
-    const classes = useStyles();
-    const [value, setValue] = React.useState(0);
-
 
     useEffect(() => {
     }, []);
